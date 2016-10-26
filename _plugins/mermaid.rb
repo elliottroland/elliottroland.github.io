240d813dc446cd0540a52963c9b07802416ca2d9
Roland Elliott <rolae@amazon.com>
2016-10-26 12:53:23 +0200

    Another fix

--- _plugins/mermaid.rb
+++ _plugins/mermaid.rb
@@ -1,8 +1,9 @@
 # A mermaid plugin: https://github.com/knsv/mermaid
 # A combination of the advice here: http://knsv.github.io/mermaid/#simple-usage-on-a-web-page
 # And the jekyll-mermaid plugin: https://github.com/jasonbellamy/jekyll-mermaid/blob/master/lib/jekyll-mermaid.rb
+# And Jekyll docs: https://jekyllrb.com/docs/plugins/#tags
 module Jekyll
-  class Mermaid < Liquid::Block
+  class Mermaid < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       super
     end
