4d488e0a346b9c699424a2340812e570625f622e
Roland Elliott <rolae@amazon.com>
2016-10-26 12:45:05 +0200

    Add mermaid with test

---
+++ _plugins/mermaid.rb
@@ -0,0 +1,15 @@
+# A mermaid plugin: https://github.com/knsv/mermaid
+# A combination of the advice here: http://knsv.github.io/mermaid/#simple-usage-on-a-web-page
+# And the jekyll-mermaid plugin: https://github.com/jasonbellamy/jekyll-mermaid/blob/master/lib/jekyll-mermaid.rb
+module Jekyll
+  class Mermaid < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      super
+    end
+
+    def render(context)
+      @config = context.registers[:site].config['mermaid']
+      "<div class=\"mermaid\">#{super}</div>"
+    end
+  end
+end
--- _wip/systems-diagrammatically-2.md
+++ _wip/systems-diagrammatically-2.md
@@ -7,3 +7,11 @@ categories: [formalisms]
 date: 2016-10-26 01:01:01 +00:00
 ---
 In our [previous post]({{ site.url }}/formalisms/2016/10/25/systems-diagrammatically.html) we made a number of distinctions between the kinds of components one can find in a system, and we talked about how to depict functions and channels. In this post we'll briefly consider how algorithmic and operational independence are depicted.
+
+{% mermaid %}
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+{% endmermaid %}
